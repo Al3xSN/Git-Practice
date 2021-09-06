e6b96037c2bce92245436f1ffe6795810392a88e
Alex Nikolov <nikolovalex3@gmail.com>
2021-09-06 19:07:21 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function changeColor() {
+    if (document.getElementById("text").style.color != "green") {
+        document.getElementById("text").style.color = "green";
+    }
+    else {
+        document.getElementById("text").style.color = "red";
+    }
+}
